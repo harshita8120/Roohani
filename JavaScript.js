354d8927ff9bbbefd0655975ec1a4420b39bd424
harshita8120 <harshita.ranjan25@gmail.com>
2025-06-14 23:29:48 +0530

    Files added

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+
+  function myFunction() {
+    alert("Request has been submitted. You'll shortly receive an E-mail.");
+   }
+
+   function msgFunction() {
+    alert("Message has been sent successfully!");
+   }
+
+
