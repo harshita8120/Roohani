31475544b2c8b0de39d1414d7ba2ccec0632d8c8
harshita8120 <harshita.ranjan25@gmail.com>
2025-12-01 19:55:16 +0530

    Separating the code for footer using JS

---
+++ scripts/common/footer.js
@@ -0,0 +1,22 @@
+const footerHTML = `
+&#128205 Central Mall, Block B, 1st Floor, GS Road, Guwahati, Assam - 781006 <br><br>
+
+&#169 2024 Roohani by Nayanjyoti Saikia. All Rights Reserved.<br>
+
+Follow us: <br>
+
+<center>
+
+<a href="https://www.instagram.com/roohanibynayanjyoti?igsh=MXJidzBjYm1zN2tqZQ==" title="roohanibynayanjyoti" target="_blank" style="margin-right:0;">
+<img src="images/Instagram_logo_2022.svg.png" alt="inst1" style="width: 20px; height:20px; float:center;"></a>
+
+<a href="https://www.facebook.com/nayan.saikia.5680/" title="Nayanjyoti Saikia" target="_blank" style="margin-right:0;">
+<img src="images/facebook.png" alt="fb" style="width: 20px; height:20px;" style="margin-right:0;"></a>
+
+<a href="https://www.instagram.com/the_travellercook?igsh=MWYzeWQ3dm50c3Rzeg==" title="the_travellercook" target="_blank" style="margin-right:0;">
+<img src="images/Instagram_logo_2022.svg.png" alt="inst2" style="width: 20px; height:20px; float:center;"></a>
+
+</center>
+`;
+
+document.querySelector('.footer-container').innerHTML=footerHTML;
