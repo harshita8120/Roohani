e1ff6dacf3c76d1162577c02612968c32e4ce1d3
harshita8120 <harshita.ranjan25@gmail.com>
2025-12-02 20:35:14 +0530

    Made footer responsive

--- scripts/common/footer.js
+++ scripts/common/footer.js
@@ -1,22 +1,22 @@
 const footerHTML = `
-&#128205 Central Mall, Block B, 1st Floor, GS Road, Guwahati, Assam - 781006 <br><br>
+<p>&#128205 Central Mall, Block B, 1st Floor, GS Road, Guwahati, Assam - 781006 </p>
 
-&#169 2024 Roohani by Nayanjyoti Saikia. All Rights Reserved.<br>
+<p id="socialPara">Follow us: </p>
 
-Follow us: <br>
+<div class="social">
 
-<center>
+<a href="https://www.instagram.com/roohanibynayanjyoti?igsh=MXJidzBjYm1zN2tqZQ==" title="roohanibynayanjyoti" target="_blank">
+<img src="images/Instagram_logo_2022.svg.png" alt="inst1"></a>
 
-<a href="https://www.instagram.com/roohanibynayanjyoti?igsh=MXJidzBjYm1zN2tqZQ==" title="roohanibynayanjyoti" target="_blank" style="margin-right:0;">
-<img src="images/Instagram_logo_2022.svg.png" alt="inst1" style="width: 20px; height:20px; float:center;"></a>
+<a href="https://www.facebook.com/nayan.saikia.5680/" title="Nayanjyoti Saikia" target="_blank">
+<img src="images/facebook.png" alt="fb"></a>
 
-<a href="https://www.facebook.com/nayan.saikia.5680/" title="Nayanjyoti Saikia" target="_blank" style="margin-right:0;">
-<img src="images/facebook.png" alt="fb" style="width: 20px; height:20px;" style="margin-right:0;"></a>
+<a href="https://www.instagram.com/the_travellercook?igsh=MWYzeWQ3dm50c3Rzeg==" title="the_travellercook" target="_blank">
+<img src="images/Instagram_logo_2022.svg.png" alt="inst2" ></a>
 
-<a href="https://www.instagram.com/the_travellercook?igsh=MWYzeWQ3dm50c3Rzeg==" title="the_travellercook" target="_blank" style="margin-right:0;">
-<img src="images/Instagram_logo_2022.svg.png" alt="inst2" style="width: 20px; height:20px; float:center;"></a>
+</div>
 
-</center>
+<span> &#169 2024 Roohani by Nayanjyoti Saikia. All Rights Reserved.</span><br>
 `;
 
 document.querySelector('.footer-container').innerHTML=footerHTML;
