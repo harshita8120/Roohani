3aa20306ef19ed0d96cab22f8c108b23842f5174
harshita8120 <harshita.ranjan25@gmail.com>
2025-12-02 19:03:05 +0530

    Made the header responsive

--- scripts/common/header.js
+++ scripts/common/header.js
@@ -1,16 +1,18 @@
 const navbarHTML = `
-<img src="images/Roohani.jpeg" alt="Roohani: Soulful indulgence by Masterchef Nayan Jyoti"   
-    style="width:300px; height:100px; float:left;">
+<img src="images/Roohani.jpeg" alt="Roohani: Soulful indulgence by Masterchef Nayan Jyoti" 
+     class="nav-logo"  >
 
-<div id="list">
+<div class="list">
 
-<a href='index.html' id='header' target='_self' title='Home'> Home </a>
-<a href='Menu.html' id='header' target='_blank' title='Menu'> Menu </a>
-<a href='Gallery.html' id='header' target='_blank' title='Gallery'> Gallery </a> 
-<a href='About.html' id='header' target='_blank' title='About'> About </a>
-<a href='Contact.html' id='header' target='_blank' title='Contact'> Contact </a> 
+<a href='index.html' target='_self' title='Home'> Home </a>
+<a href='Menu.html' target='_blank' title='Menu'> Menu </a>
+<a href='Gallery.html' target='_blank' title='Gallery'> Gallery </a> 
+<a href='About.html' target='_blank' title='About'> About </a>
+<a href='Contact.html' target='_blank' title='Contact'> Contact </a> 
 
 </div>
+
+<i class="ri-menu-3-fill"></i>
 `;
 
 document.querySelector('.navbar-container').innerHTML=navbarHTML;
