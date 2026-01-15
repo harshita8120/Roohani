f55c2f41912ce0c4bcea5ecc9f6c2e50f6dd1748
harshita8120 <harshita.ranjan25@gmail.com>
2026-01-15 21:34:32 +0530

    Added readme file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Roohani - Restaurant Website Project
+
+<b>DISCLAIMER:</b> <i>This project is intendet for educational purpose only. Any information provided or statements of any person mentioned may not be accurate or factually correct. The source of most of the data (texts and images) are the social media links mentioned on the project website.</i>
+
+<b>Description:</b> This project is a website for a restaurant named "Roohani", a restaurant established in 2024 by Chef Nayanjyoti Saikia. It serves as a digital storefront to showcase the restaurant's ambiance and culinary offerings.
+
+<b>Tech Stack:</b>
+- Programming Languages: HTML, CSS and JavaScript
+- Third-Party Service: Formspree (for form submissions)
+
+<b>Use of AI:</b> 
+- Tools used: Gemini Pro and ChatGPT (Basic)
+- They are mostly used for debugging, text generation (feedback and about page) and image generation (image of the restaurant's team).
+
+<b>Timeline:</b> The core structure was completed in the first semester mainly using CSS and HTML. As I advanced my skills, I revisited the project to significantly improve the UI/UX and add new features.
--- scripts/common/header.js
+++ scripts/common/header.js
@@ -3,16 +3,45 @@ const navbarHTML = `
      class="nav-logo"  >
 
 <div class="list">
-
-<a href='index.html' target='_self' title='Home'> Home </a>
-<a href='Menu.html' target='_blank' title='Menu'> Menu </a>
-<a href='Gallery.html' target='_blank' title='Gallery'> Gallery </a> 
-<a href='About.html' target='_blank' title='About'> About </a>
-<a href='Contact.html' target='_blank' title='Contact'> Contact </a> 
-
+     <a href='index.html' target='_self' title='Home'> Home </a>
+     <a href='Menu.html' target='_blank' title='Menu'> Menu </a>
+     <a href='Gallery.html' target='_blank' title='Gallery'> Gallery </a> 
+     <a href='About.html' target='_blank' title='About'> About </a>
+     <a href='Contact.html' target='_blank' title='Contact'> Contact </a> 
 </div>
 
-<i class="ri-menu-3-fill"></i>
+<i class="ri-menu-3-fill menuButton"></i>
+
+<div class="navbar-vertical">
+     <i class="ri-close-fill crossButton"></i>
+     <div class="list-vertical">
+          <a href='index.html' target='_self' title='Home'> Home </a>
+          <hr>
+          <a href='Menu.html' target='_blank' title='Menu'> Menu </a>
+          <hr>
+          <a href='Gallery.html' target='_blank' title='Gallery'> Gallery </a> 
+          <hr>
+          <a href='About.html' target='_blank' title='About'> About </a>
+          <hr>
+          <a href='Contact.html' target='_blank' title='Contact'> Contact </a> 
+          <hr>
+     </div>
+</div>
 `;
 
 document.querySelector('.navbar-container').innerHTML=navbarHTML;
+
+const menuBtn = document.querySelector('.menuButton');
+const crossBtn = document.querySelector('.crossButton');
+
+menuBtn.addEventListener('click', () => {
+     document.querySelector('.navbar-vertical').classList.remove('navbar-vertical-hide');
+     document.querySelector('.navbar-vertical').classList.add('navbar-vertical-visible');
+     
+});
+
+crossBtn.addEventListener('click', () => {
+     document.querySelector('.navbar-vertical').classList.remove('navbar-vertical-visible');
+     document.querySelector('.navbar-vertical').classList.add('navbar-vertical-hide');
+});
+
