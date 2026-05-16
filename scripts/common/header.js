03c1f259729f2ff6ce4cbebe9927f4fd0f47b719
harshita8120 <harshita.ranjan25@gmail.com>
2026-05-16 15:51:49 +0530

    Removed title tags in the navbar menu

--- scripts/common/header.js
+++ scripts/common/header.js
@@ -3,11 +3,11 @@ const navbarHTML = `
      class="nav-logo">
 
 <div class="list">
-     <a href='index.html' target='_self' title='Home'> Home </a>
-     <a href='Menu.html' target='_blank' title='Menu'> Menu </a>
-     <a href='Gallery.html' target='_blank' title='Gallery'> Gallery </a> 
-     <a href='About.html' target='_blank' title='About'> About </a>
-     <a href='Contact.html' target='_blank' title='Contact'> Contact </a> 
+     <a href='index.html' target='_self'> Home </a>
+     <a href='Menu.html' target='_blank'> Menu </a>
+     <a href='Gallery.html' target='_blank'> Gallery </a> 
+     <a href='About.html' target='_blank'> About </a>
+     <a href='Contact.html' target='_blank'> Contact </a> 
 </div>
 
 <i class="ri-menu-3-fill menuButton"></i>
